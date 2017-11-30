7846b788f0070e867a6f29f7eba1c9bab5f585bc
Berk Özdemir <berkozdemir@Berks-MBP.lan>
2017-11-30 15:30:03 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# random-youtube-downloader
-Random keyword search / download from youtube.
+# youtuberandomdownloader.py
+
+Hi everyone!
+
+This is my first program made with python. I just started learning Python (and it's my first language), so if you have any suggestions to the code, please help me make this better!
+
+I worked on this program because I want to use youtube videos in my ongoing art projects.
+
+It's also my first time using git, so if you see me screwing up with the pull requests and other things, please send me an email! 
+
+With this small program, you can get urls of up to 200 random videos from youtube (you can change the limit in the code easily) and download them. You can choose between 4 languages (english, turkish, german, japanese, or all of them), or you can type your own keyword to get the videos.
+
+
+### Usage:
+
+* Add your API key to apikey.txt in the main directory (first, if you don't have, get your own API token from Google.)
+* You should have youtube_dl module installed. (run "pip install youtube_dl")
+* Also you may need to install youtube-dl, ffmpeg on your computer.
+* Run the script with python3.
+* Follow the steps.
+* All video infos will be printed as a json file to the "json" subfolder, and videos will be downloaded to "/output/yourjsonname" folder.
+
+
+### Things to add:
+
+* More download options (right now it's only 192 kbps mp3 only)
+* Handling errors in downloads section.
+* Different languages and keyword collections
+* Maybe using online apis to get random search keywords instead of using txt files.
+* Changing the duration in json to minutes:seconds format.
+* Adding other video streaming services (Vimeo, Pornhub etc.)
+* Playlist search - download.
+* I'm also planning to add a audio/video analysis and trimming tool. But maybe I should do them as separate programs.
+
+### The things that I'm not very happy with:
+
+* It seems like with Google's API I can't search for least watched videos, and the search results are not completely random, it usually finds the most popular result. If you know any ways to dig deeper on Youtube, please contact me.
+
---
+++ randomyoutubedownloader.py
@@ -0,0 +1,241 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from datetime import datetime
+import random
+import urllib.request
+import string
+import json
+import urllib.parse
+import os
+import time
+import youtube_dl
+
+def Main():    
+    #keyword select
+    print('How would you like to search for videos?')
+    print('1. Use english words')
+    print('2. Use turkish words')
+    print('3. Use german words')
+    print('4. Use japanese words')
+    print('5. Use all languages listed above')
+    print('6. Type your own keyword')
+    
+    while True:
+        queryChoice = input('Select: ')
+        if queryChoice == "1":
+            print('Imma gonna search random english words!')
+            keyword = "English"
+            break
+        elif queryChoice == "2":
+            print('Imma gonna search random turkish words!')
+            keyword = "Turkish"
+            break
+        elif queryChoice == "3":
+            print('Imma gonna search random german words!')
+            keyword = "German"
+            break
+        elif queryChoice == "4":
+            print('Imma gonna search random japanese words!')
+            keyword = "Japanese"
+            break
+        elif queryChoice == "5":
+            print('Imma gonna search random words from all languages I have!')
+            keyword = "Random"
+            break
+        elif queryChoice == "6":
+            print('So what do you want to search for?')
+            keyword = input("Your keyword: ")
+            break
+        else:
+            print('Dude... Please... Select... Something... Between... 1-6')
+
+    #videoAmount select
+    print('How many videos would you like to get? Max 200')
+    while True:
+        userChoice = int(input("Amount: "))
+        if userChoice in range(1,201):
+            print('{} videos then!'.format(userChoice))
+            videoAmount = userChoice
+            break
+        else:
+            print("Please write something between 1 and 200.")
+    print('Video amount is {}'.format(videoAmount))
+
+    #videoDuration select
+    print('Do you have any preference for duration?')
+    print('1. Short')
+    print('2. Medium')
+    print('3. Long')
+    print('4. Naah, just give me random videos!')
+
+    while True:
+        userChoice = int(input('Duration: '))
+        if userChoice == 1:
+            videoDuration = "short"
+            break
+        elif userChoice == 2:
+            videoDuration = "medium"
+            break
+        elif userChoice == 3:
+            videoDuration = "long"
+            break
+        elif userChoice == 4:
+            videoDuration = "any"
+            break
+        else:
+            print('Aaaaaargh, please choose 1, 2, 3 or 4. PLEASE.')
+
+    print("You chose {}".format(videoDuration))
+            
+    print("----------------------------")
+    print("Language/Keyword: {}".format(keyword))
+    print("Video Amount: {}".format(videoAmount))
+    print("Durations of Videos: {}".format(videoDuration))
+    print('Are you sure with this setting? Y/N')
+    print('Warning: If you choose No, you are going to turn back to the beginning.')
+    print("----------------------------")
+
+    yesorno = input("Y or N: ").lower()
+    
+    if yesorno.startswith("y"):
+        ytfetch(queryChoice,videoAmount,videoDuration,keyword)
+    else:
+        Main()
+
+def Query(queryChoice):
+    if queryChoice == "1":
+        return random.choice(open('languages/english.txt').read().splitlines())
+    elif queryChoice == "2":
+        return random.choice(open('languages/turkish.txt').read().splitlines())
+    elif queryChoice == "3":
+        return random.choice(open('languages/german.txt').read().splitlines())
+    elif queryChoice == "4":
+        return random.choice(open('languages/japanese.txt').read().splitlines())
+    elif queryChoice == "5":
+        _files = os.listdir('languages/')
+        number = random.randint(0, len(_files) - 1)
+        file_ = _files[number]
+        query = random.choice(open('languages/{}'.format(file_)).read().splitlines())
+        return query
+    
+
+def ytfetch(queryChoice,videoAmount,videoDuration,keyword):
+    apiKey = open('apikey.txt').read()
+    iteration = 1
+    dumpvideos = {}
+    videolist = []
+
+    print("Do you want to give a name to the json file?")
+    
+    jsonQ = input("Y or N: ").lower()
+                       
+    if jsonQ.startswith("y"):
+        jsonname = input("Enter a file name: ")
+    else:
+        jsonname = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+      
+    while iteration <= videoAmount:
+            #parsing url
+            if queryChoice in ["1","2","3","4","5"]:
+                query_ = Query(queryChoice)
+                maxresults = 1
+            else:
+                query_ = keyword
+                maxresults = videoAmount   
+                
+            query2_ = urllib.parse.quote_plus(query_)    
+
+            urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&videoDuration={}&q={}".format(apiKey,maxresults,videoDuration,query2_)
+            webURL = urllib.request.urlopen(urlData)
+            data = webURL.read()
+            encoding = webURL.info().get_content_charset('utf-8')
+            results = json.loads(data.decode(encoding))
+            
+            if 'items' in results:
+                if results['items'] == "":
+                    iteration = iteration
+                else:
+                    for data in results['items']:
+                        videoId = data['id']['videoId']
+                        videoName = data['snippet']['title']
+                        channelId = data['snippet']['channelId']
+                        channelName = data['snippet']['channelTitle']
+                        description = data['snippet']['description']
+                        iteration = int(iteration)
+                        # Another API request to get the duration:
+                        contentDetails = json.loads(urllib.request.urlopen("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={}&key={}".format(videoId,apiKey)).read())
+                        duration = contentDetails['items'][0]['contentDetails']['duration']        
+                        update = {str(iteration): {'query': query_, 'videoId': videoId, 'videoName': videoName, 'channelId': channelId, 'channelName': channelName, 'description': description, 'duration': duration}}
+                        print("{}. {} - {}".format(iteration, query_, videoName))
+                        dumpvideos.update(update)            
+                        videolist.append(videoId)
+                        iteration += 1
+    
+
+    with open('./json/{}.json'.format(jsonname), 'a') as outfile: 
+        dumpdump = {"Search": {"searchDate": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "queryChoice:": keyword, "videoAmount": videoAmount, "videoDuration": videoDuration}}
+        dumpdump.update({"Videos": dumpvideos})
+        json.dump(dumpdump, outfile, indent=4, ensure_ascii = False)
+
+    print("Finally, json file is ready!")
+    print("So, would you like to download the videos? Y/N")
+    dlChoice = input("Select: ").lower()
+
+    if dlChoice.startswith("y"):
+        ytDownload(jsonname,videolist)
+    else:
+        print("See ya!")
+
+
+    
+def ytDownload(jsonname, videolist):    
+    
+    os.mkdir("output/{}".format(jsonname))
+
+    class MyLogger(object):
+        def debug(self, msg):
+            pass
+
+        def warning(self, msg):
+            pass
+
+        def error(self, msg):
+            print(msg)
+
+
+    def my_hook(d):
+        
+        if d['status'] == 'finished':
+            print('Done downloading, now converting ...')
+
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+ 
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        os.chdir("output/{}".format(jsonname))
+        ydl.download(videolist)
+
+
+
+# Program Starts Here
+
+print('Welcome my ladies and gentlemen to this wonderful youtube grabber/download software!')
+print('============== version 1.something =================')
+
+Main()
+
+
+
+
+
